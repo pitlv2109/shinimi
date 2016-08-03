766abc323a4672ea92ddfef0a59c322f2ee0722f
Phillip Quy Le <pitlv2109@Phillips-MacBook-Pro.local>
2016-08-03 12:50:14 -0500

    make app.js modular

---
+++ bot.js
@@ -0,0 +1,88 @@
+'use strict'
+
+const FB = require('./facebook.js');
+const Config = require('./const.js');
+
+let Wit = null;
+let log = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  log = require('../').log;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
+}
+
+// ----------------------------------------------------------------------------
+// Wit.ai bot specific code
+
+// First entity
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+// Our bot actions
+const actions = {
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return FB.fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
+  },
+  // You should implement your custom actions here
+  // See https://wit.ai/docs/quickstart
+  getForecast({context, entities}) {
+  return new Promise(function(resolve, reject) {
+    var location = firstEntityValue(entities, "location")
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return resolve(context);
+  });
+},
+};
+
+const getWit = () => {
+  return new Wit(Config.WIT_TOKEN, actions);
+};
+
+exports.getWit = getWit;
+
+// bot testing mode
+// http://stackoverflow.com/questions/6398196
+if (require.main === module) {
+  console.log("Bot testing mode.");
+  const client = getWit();
+  client.interactive();
+}
